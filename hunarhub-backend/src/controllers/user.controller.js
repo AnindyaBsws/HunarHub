744f41740fdbbe2aea5c3829d155c2f901a1ac46
AnEye <shakhar004@gmail.com>
2026-04-14 16:52:08 +0530

    feat: User log in tested successfully api route, password hashing configured

--- hunarhub-backend/src/controllers/user.controller.js
+++ hunarhub-backend/src/controllers/user.controller.js
@@ -1,3 +1,6 @@
+//Import Libraries
+import bcrypt from 'bcrypt';
+
 //Imports
 import prisma from '../config/prisma.js';
 
@@ -8,35 +11,114 @@ function testUser(req,res){
 
 //........Register User...........
 async function registerUser(req,res){
+    //....Flow.....
+    //1. req.body
+    //2.Validation
+    //3.Try-Catch block starts
+    //4.Existing Mail Checker
+    //5.Password Hashing
+    //6.Create User table, response and error Handling
+
     const { name,email,password } = req.body;
 
+    //Validation of Required Fields
     if(!name || !email || !password){
         return res.status(400).json({
             message: 'All Fields are Required!!'
         });
     }
 
-    
     try {
+        //Existing Email Checker
+        const existingUser = await prisma.user.findUnique({
+            where: { email }
+        });
+        if(existingUser){
+            return res.status(400).json({
+                message: 'This Email is already registered'
+            });
+        }
+
+        //Password Hashing
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+
+        //Create Table
         await prisma.user.create({
             data: {
                 name,
                 email,
-                password
+                password: hashedPassword
             }
         });
 
+        //Response
         res.status(201).json({
         message: 'User Created Successfully.'
         });
 
     } catch (error) {
+        //Error Handling
+        console.error(error);
         res.status(500).json({
-            message: 'Something went wrong when User Registering!!'
+            message: 'Server Error'
+        });
+    }
+}
+
+
+async function loginUser(req,res){
+    // ...flow...
+    // 1.Get (mail+pass)
+    // 2.Validation
+    // 3.find user in Db
+    // 4.compare pass(bcrypt)
+    // 5.Send Response
+    // 6.Error Handling
+
+    const { email,password } = req.body;
+    const cleanEmail = email.trim().toLowerCase();
+
+    //Validation
+    if(!email || !password){
+        return res.status(400).json({
+            message: 'All fields are required'
         });
     }
 
 
+    try {
+        //Find user in DB
+        const user = await prisma.user.findUnique({
+            where: { email: cleanEmail }
+        });
+        if(!user){
+            return res.status(400).json({
+                message: 'The user does not exist'
+            });
+        }
+
+        //if User found, Compare bcrypt password
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({
+                message: 'Invalid Credentials.'
+            });
+        }
+
+        //Success Message
+        return res.status(200).json({
+            message: 'Login Successful.'
+        });
+
+    } catch (error) {
+        //Error Handling
+        console.error(error);
+        res.status(500).json({
+            message: 'Server Error'
+        });
+    }
 }
 
-export { testUser,registerUser };
+export { testUser,registerUser,loginUser };
--- hunarhub-backend/src/routes/user.routes.js
+++ hunarhub-backend/src/routes/user.routes.js
@@ -3,7 +3,7 @@ import express from 'express';
 
 
 //Import controllers
-import { testUser,registerUser } from '../controllers/user.controller.js';
+import { testUser,registerUser,loginUser } from '../controllers/user.controller.js';
 
 //Create router
 const router = express.Router();
@@ -11,8 +11,13 @@ const router = express.Router();
 //Create test route
 router.get('/test-user', testUser);
 
+
+
+
 //Create registerUser route
 router.post('/register', registerUser);
+//Create LoginUser route
+router.post('/login', loginUser);
 
 
 //export route
